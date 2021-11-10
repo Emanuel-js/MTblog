2ca7d17b46dca157e7457d6ac8ba30c0428af0a8
sitota49 <se.sitota.ezra@gmail.com>
2021-11-10 20:53:52 +0300

    blogs endpoints done

--- server/routes/route.js
+++ server/routes/route.js
@@ -7,13 +7,28 @@ const {
     getAllAuthors,
     updateAuthor,
     deleteAuthor,
+    
+
+} = require('../controllers/authors');
+
+
+const {
     addCategory,
     getAllCategories,
     getCategoryById,
     updateCategory,
     deleteCategory
+} = require('../controllers/categories');
 
-} = require('../controllers/authors');
+
+const {
+    addBlog,
+getAllBlogs,
+    getBlogById,
+    updateBlog,
+    deleteBlog
+} = require('../controllers/blogs');
+
 
 
 const router= express.Router();
@@ -26,9 +41,16 @@ router.delete('/author/:id', deleteAuthor);
 
 // category end points
 
-router.get('/category',getAllCategories)
+router.get('/category', getAllCategories)
 router.get('/category/:id', getCategoryById);
 router.post('/category', addCategory);
 router.put('/category/:id', updateCategory);
 router.delete('/category/:id', deleteCategory);
+
+//blog end points
+router.get('/blogs', getAllBlogs)
+router.get('/blogs/:id', getBlogById);
+router.post('/blogs', addBlog);
+router.put('/blogs/:id', updateBlog);
+router.delete('/blogs/:id', deleteBlog);
 module.exports = router;
